f4b0b567edee208fa2d2b8d5ad8404fc9582bedf
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-11-23 18:14:41 +0900

    update

--- ymir/build.zig
+++ ymir/build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
-
     const surtr = b.addExecutable(.{
         .name = "BOOTX64.EFI",
         .root_source_file = b.path("surtr/boot.zig"),
@@ -14,4 +13,14 @@ pub fn build(b: *std.Build) void {
         .linkage = .static,
     });
     b.installArtifact(surtr);
+
+    // EFI directory
+    const out_dir_name = "img";
+    const install_surtr = b.addInstallFile(
+        surtr.getEmittedBin(),
+        b.fmt("{s}/efi/boot/{s}", .{ out_dir_name, surtr.name }),
+    );
+    install_surtr.step.dependOn(&surtr.step);
+    b.getInstallStep().dependOn(&install_surtr.step);
 }
+
